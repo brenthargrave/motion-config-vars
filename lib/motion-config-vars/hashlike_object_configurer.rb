550dfc4117a4ead57c6bf9c62aad864bd4ed9d18
Brent Hargrave <brent@brent.is>
2013-03-22 14:31:43 -0400

    use RuntimeError until I sort out ConfigurationError

--- lib/motion-config-vars/hashlike_object_configurer.rb
+++ lib/motion-config-vars/hashlike_object_configurer.rb
@@ -1,4 +1,8 @@
+class ConfigurationError < Exception; end;
+
 class HashlikeObjectConfigurer
+
+  attr_reader :hashlike_object, :config_vars_data, :config_name_for_facet_named
 
   def initialize options={}
     @hashlike_object = options[:hashlike_object]
@@ -7,12 +11,39 @@ class HashlikeObjectConfigurer
     raise ArgumentError, "'config_vars_data' missing" unless @config_vars_data
     @config_name_for_facet_named = options[:config_name_for_facet_named]
     raise ArgumentError, "'config_name_for_facet_named' missing" unless @config_name_for_facet_named
-    unless @config_name_for_facet_named.respond_to? :call
+    self.validate_config_name_for_facet_named_is_closure
+    self.validate_hashlike_object_is_hashlike
+    self.validate_all_configured_facets_are_specified
+  end
+
+protected
+
+  def validate_config_name_for_facet_named_is_closure
+    unless self.config_name_for_facet_named.respond_to? :call
       raise ArgumentError, "'config_name_for_facet_named' must be a closure"
     end
-    unless @hashlike_object.respond_to?(:[]=)
+  end
+
+  def validate_hashlike_object_is_hashlike
+    unless self.hashlike_object.respond_to?(:[]=)
       raise ArgumentError, "hashlike_object doesn't respond to []="
     end
+  end
+
+  def validate_all_configured_facets_are_specified
+    unless self.all_configured_facets_are_specified?
+      raise RuntimeError, "configured facet not specified: API_ENV"
+    end
+  end
+
+  def all_configured_facets_are_specified?
+    self.configured_facets_names.all? do |facet_name|
+      self.config_name_for_facet_named.call facet_name
+    end
+  end
+
+  def configured_facets_names
+    self.config_vars_data.keys
   end
 
 end
--- spec/hashlike_object_configurer_spec.rb
+++ spec/hashlike_object_configurer_spec.rb
@@ -1,5 +1,7 @@
 describe HashlikeObjectConfigurer do
 
+  before{ @described_class = HashlikeObjectConfigurer }
+
   describe "initialization" do
 
     before do
@@ -20,7 +22,7 @@ describe HashlikeObjectConfigurer do
     [:hashlike_object, :config_vars_data, :config_name_for_facet_named].each do |attr|
       it "raises ArgumentError without #{attr}" do
         lambda do
-          HashlikeObjectConfigurer.new @options.tap { |options| options.delete(attr) }
+          @described_class.new @options.tap { |options| options.delete(attr) }
         end.should.raise ArgumentError, "'#{attr}' missing"
       end
     end
@@ -28,7 +30,7 @@ describe HashlikeObjectConfigurer do
     it "raises ArgumentError if :config_name_for_facet_named isn't a closure" do
       @options[:config_name_for_facet_named] = "not-a-closure"
       lambda do
-        HashlikeObjectConfigurer.new @options
+        @described_class.new @options
       end.should.raise ArgumentError, "'config_name_for_facet_named' must be a closure"
     end
 
@@ -36,8 +38,15 @@ describe HashlikeObjectConfigurer do
       unhashlike_object = Object.new
       @options[:hashlike_object] = unhashlike_object
       lambda do
-        HashlikeObjectConfigurer.new @options
+        @described_class.new @options
       end.should.raise ArgumentError, "hashlike_object must respond to []="
+    end
+
+    it "raises RuntimeError unless all configured facets are specified" do
+      @options[:config_name_for_facet_named] = lambda { Hash.new }
+      lambda do
+        @described_class.new @options
+      end.should.raise RuntimeError, "configured facet not specified: API_ENV"
     end
 
   end
