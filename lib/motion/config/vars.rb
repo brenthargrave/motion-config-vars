3068f4199492be1215cb21a6e5f01d38dc410cd1
Brent Hargrave <brent@brent.is>
2013-03-22 12:41:20 -0400

    generate gem skeleton with bundler

---
+++ lib/motion/config/vars.rb
@@ -0,0 +1,9 @@
+require "motion/config/vars/version"
+
+module Motion
+  module Config
+    module Vars
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/motion/config/vars/version.rb
@@ -0,0 +1,7 @@
+module Motion
+  module Config
+    module Vars
+      VERSION = "0.0.1"
+    end
+  end
+end
