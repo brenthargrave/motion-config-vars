7fb71b2ac733ddf0ca4e5648f62fc873133326a6
Brent Hargrave <brent@brent.is>
2013-03-23 08:47:05 -0400

    reuse HashlikeObjectConfigurer in confi_vars_loader.rb

---
+++ lib/motion-config-vars/embed/config_vars_loader.rb
@@ -0,0 +1,16 @@
+if path = NSBundle.mainBundle.pathForResource("app", ofType:"yml")
+
+  vars_data = YAML.load File.read path
+
+  HashlikeObjectConfigurer.new({
+    config_vars_data: vars_data,
+    hashlike_object: ENV,
+    config_name_for_facet_named: lambda { |facet_name|
+      NSBundle.mainBundle.objectForInfoDictionaryKey facet_name
+    }
+  }).perform!
+
+else
+  puts "WARNING: main bundle missing app.yml"
+end
+
